fd11d675cc4a8356d87a2281eb41814c5e2abd6e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-06-01 13:02:02 +0200

    feat: add styles for search form

---
+++ src/pages/Expenses/components/SearchForm/index.tsx
@@ -0,0 +1,40 @@
+import { styled } from "styled-components";
+
+
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
+
+  input {
+    flex: 1;
+    border-radius: 0.25rem;
+    border: 0;
+    background-color: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-200']};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${props => props.theme['gray-500']};
+    }
+  }
+
+  button {
+    background-color: ${props => props.theme['green-700']};
+    color: ${props => props.theme['gray-900']};
+    padding: 1rem 2rem;
+    border: 0;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    font-weight: 600;
+    transition: filter 0.2s;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.9);
+      background-color: ${props => props.theme['yellow-800']};
+      transition: background-color 0.2s;
+    }
+  }
+`;
